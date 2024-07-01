5de232d90a05bccd62fd1d5eaeb5ce5090055587
vaibhavsraut <vaibhav.raut851@gmail.com>
2024-07-01 12:29:53 +0530

    Changed StudentServices method parameters

--- JavaQuizApplication/src/com/velocity/quizapp/student/StudentServices.java
+++ JavaQuizApplication/src/com/velocity/quizapp/student/StudentServices.java
@@ -6,7 +6,7 @@ import com.velocity.quizapp.admin.Question;
 import com.velocity.quizapp.database.DatabaseServices;
 
 public interface StudentServices extends DatabaseServices{
-	void registerStudent(Student student);
+	void registerStudent(String firstName,String lastName,String userName,String password,String city,String email,String mobile_number);
 	Student login(String username, String password);
 	void startQuiz(Student student);
 	List<Question> getAllQuestions();
